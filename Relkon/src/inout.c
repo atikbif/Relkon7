9f63f8b75f7b539742498c72792b642ebf1e2702
Roman Pelevin <kontel@mail.ru>
2016-05-25 09:56:44 +0300

    счётчики входных запросов по RS485 на каналах PC, PU, PR, MMBЭ

--- Relkon/src/inout.c
+++ Relkon/src/inout.c
@@ -38,6 +38,8 @@ extern plc_stat _Sys;
 volatile unsigned char prot_enable=1;
 request req_mb;
 
+unsigned short rx_req_cnt_mmb=0;
+
 portTickType MBxLastExecutionTime;
 
 void InOutTask( void *pvParameters )
@@ -104,97 +106,99 @@ void InOutTask( void *pvParameters )
 			exchange_work();
 			if((rx_mod_cnt)&&(get_mmb_tmr() >= 15000)&&(prot_enable))
 			{
-				if(((rx_mod_buf[0]==_Sys.Adr)||(rx_mod_buf[0]==0))&&(GetCRC16(rx_mod_buf,rx_mod_cnt)==0))
-				switch(rx_mod_buf[1])
-				{
-					case 0x01:
-						req_mb.addr = ((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt = ((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_coils(&req_mb);write_module(tmp);break;
-					case 0x02:
-						req_mb.addr = ((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt = ((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_dinputs(&req_mb);write_module(tmp);break;
-					case 0x03:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_holdregs(&req_mb);write_module(tmp);break;
-					case 0x04:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=read_inregs(&req_mb);write_module(tmp);break;
-					case 0x05:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = write_single_coil(&req_mb);write_module(tmp);break;
-					case 0x06:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = write_single_reg(&req_mb);write_module(tmp);break;
-					case 0x10:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=write_multi_regs(&req_mb);write_module(tmp);break;
-					case 0x0F:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=write_multi_coils(&req_mb);write_module(tmp);break;
-					case 0xA0:tmp=get_software_ver(&req_mb);write_module(tmp);break;
-					case 0xA1:tmp=get_hardware_ver(&req_mb);write_module(tmp);break;
-					case 0xA2:tmp=get_can_name(&req_mb);write_module(tmp);break;
-					case 0xB0:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_io(&req_mb);write_module(tmp);break;
-					case 0xB1:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = write_io(&req_mb);write_module(tmp);break;
-					case 0xD0:
-						req_mb.addr = rx_mod_buf[2];req_mb.cnt = rx_mod_buf[3];
-						tmp=read_mem(&req_mb);write_module(tmp);break;
-					case 0xD1:
-						req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
-						tmp=read_time(&req_mb);write_module(tmp);break;
-					case 0xD3:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_frmem(&req_mb);write_module(tmp);break;
-					case 0xD4:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_ram(&req_mb);write_module(tmp);break;
-					case 0xD5:
-						req_mb.laddr=((unsigned long)rx_mod_buf[2]<<24) | ((unsigned long)rx_mod_buf[3]<<16) | ((unsigned long)rx_mod_buf[4]<<8) |rx_mod_buf[5];
-						req_mb.cnt=((unsigned int)rx_mod_buf[6]<<8) | rx_mod_buf[7];
-						tmp = read_flash(&req_mb);write_module(tmp);break;
-					case 0xD6:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp = read_preset(&req_mb);write_module(tmp);break;
-					case 0xE0:
-						req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
-						tmp = write_mem(&req_mb);write_module(tmp);break;
-					case 0xE1:
-						req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
-						tmp=write_time(&req_mb);write_module(tmp);break;
-					case 0xE3:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=write_frmem(&req_mb);write_module(tmp);break;
-					case 0xE4:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=write_ram(&req_mb);write_module(tmp);break;
-					case 0xE5:
-						if(EXCHANGE) {req_mb.addr=rx_mod_buf[2];exch_answer(&req_mb);}
-						else{tmp=exchange_cmd(&req_mb);if(tmp) write_module(tmp);}
-						break;
-					case 0xE6:
-						req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
-						req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
-						tmp=write_preset(&req_mb);write_module(tmp);break;
-					case 0xFE:reset_cmd();break;
+				if(((rx_mod_buf[0]==_Sys.Adr)||(rx_mod_buf[0]==0))&&(GetCRC16(rx_mod_buf,rx_mod_cnt)==0)) {
+					rx_req_cnt_mmb++;
+					switch(rx_mod_buf[1])
+					{
+						case 0x01:
+							req_mb.addr = ((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt = ((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_coils(&req_mb);write_module(tmp);break;
+						case 0x02:
+							req_mb.addr = ((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt = ((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_dinputs(&req_mb);write_module(tmp);break;
+						case 0x03:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_holdregs(&req_mb);write_module(tmp);break;
+						case 0x04:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=read_inregs(&req_mb);write_module(tmp);break;
+						case 0x05:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = write_single_coil(&req_mb);write_module(tmp);break;
+						case 0x06:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = write_single_reg(&req_mb);write_module(tmp);break;
+						case 0x10:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=write_multi_regs(&req_mb);write_module(tmp);break;
+						case 0x0F:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=write_multi_coils(&req_mb);write_module(tmp);break;
+						case 0xA0:tmp=get_software_ver(&req_mb);write_module(tmp);break;
+						case 0xA1:tmp=get_hardware_ver(&req_mb);write_module(tmp);break;
+						case 0xA2:tmp=get_can_name(&req_mb);write_module(tmp);break;
+						case 0xB0:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_io(&req_mb);write_module(tmp);break;
+						case 0xB1:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = write_io(&req_mb);write_module(tmp);break;
+						case 0xD0:
+							req_mb.addr = rx_mod_buf[2];req_mb.cnt = rx_mod_buf[3];
+							tmp=read_mem(&req_mb);write_module(tmp);break;
+						case 0xD1:
+							req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
+							tmp=read_time(&req_mb);write_module(tmp);break;
+						case 0xD3:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_frmem(&req_mb);write_module(tmp);break;
+						case 0xD4:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_ram(&req_mb);write_module(tmp);break;
+						case 0xD5:
+							req_mb.laddr=((unsigned long)rx_mod_buf[2]<<24) | ((unsigned long)rx_mod_buf[3]<<16) | ((unsigned long)rx_mod_buf[4]<<8) |rx_mod_buf[5];
+							req_mb.cnt=((unsigned int)rx_mod_buf[6]<<8) | rx_mod_buf[7];
+							tmp = read_flash(&req_mb);write_module(tmp);break;
+						case 0xD6:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp = read_preset(&req_mb);write_module(tmp);break;
+						case 0xE0:
+							req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
+							tmp = write_mem(&req_mb);write_module(tmp);break;
+						case 0xE1:
+							req_mb.addr=rx_mod_buf[2];req_mb.cnt=rx_mod_buf[3];
+							tmp=write_time(&req_mb);write_module(tmp);break;
+						case 0xE3:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=write_frmem(&req_mb);write_module(tmp);break;
+						case 0xE4:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=write_ram(&req_mb);write_module(tmp);break;
+						case 0xE5:
+							if(EXCHANGE) {req_mb.addr=rx_mod_buf[2];exch_answer(&req_mb);}
+							else{tmp=exchange_cmd(&req_mb);if(tmp) write_module(tmp);}
+							break;
+						case 0xE6:
+							req_mb.addr=((unsigned short)rx_mod_buf[2]<<8) | rx_mod_buf[3];
+							req_mb.cnt=((unsigned short)rx_mod_buf[4]<<8) | rx_mod_buf[5];
+							tmp=write_preset(&req_mb);write_module(tmp);break;
+						case 0xFE:reset_cmd();break;
+					}
 				}
 				rx_mod_cnt=0;
 			}
