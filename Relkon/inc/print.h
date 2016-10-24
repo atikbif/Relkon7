96fb2a19dbbff29acac75a51455d26bc034463dc
Roman Pelevin <kontel@mail.ru>
2016-10-24 10:23:07 +0300

    added print_sms_var function

--- Relkon/inc/print.h
+++ Relkon/inc/print.h
@@ -8,6 +8,7 @@ void print_time(unsigned char str_num,unsigned char pos,unsigned char type);
 void print_diagn(void);
 unsigned char print_long_buf(unsigned long val,unsigned char* ptr);
 unsigned char print_short_buf(short val,unsigned char* ptr);
+void print_sms_var(short value, unsigned char *sms_buf, unsigned char width, unsigned char point_pos);
 unsigned char print_ip_buf(unsigned char ip1, unsigned char ip2, unsigned char ip3, unsigned char ip4, unsigned char* ptr);
 void print_edit(void* val,unsigned char str_num,unsigned char pos,unsigned char width,unsigned char point,unsigned char type);
 void print_edit_ee(unsigned short ind,unsigned char str_num,unsigned char pos,unsigned char width,unsigned char point,unsigned char type);
